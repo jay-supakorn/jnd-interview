1aa6036a3c98b71c2ac54311ec2b867477df5498
Supakorn S <supakorn.sangroung@gmail.com>
2021-09-14 23:23:32 +0700

    change nav bar login and register

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,9 +11,9 @@
     </head>
     <body >
         {{-- class="antialiased" --}}
-        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+        <div class="flex items-top justify-center py-4">
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="px-6 py-4">
                     @auth
                         <a href="{{ url('/home') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Home</a>
                     @else
